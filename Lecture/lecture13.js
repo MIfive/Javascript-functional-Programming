e5cdfadceb86ff9ff9d6342ac7a361ba1994f8d3
Redwan <rkprogramming2@gmail.com>
2019-02-15 00:10:21 +0600

    created a new file called lecture13.js for reduce function implementation

---
+++ Lecture/lecture13.js
@@ -0,0 +1,25 @@
+// Reduce function implementation
+
+
+var arr = [5,36,6,7,4,6,78];
+
+// normal way to sum of an array
+
+// sum=0;
+
+// for(var i=0; i<arr.length; i++){
+//     sum+=arr[i];
+// }
+
+// console.log(sum);
+
+
+//Using reduce method to sum of an array
+
+var result = arr.reduce(function(a,b){
+    return a+b;
+});
+
+console.log(result);
+
+
