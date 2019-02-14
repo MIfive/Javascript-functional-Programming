9c63d28795e97d278c5eeea14d00742cd3448b17
Redwan <rkprogramming2@gmail.com>
2019-02-14 21:15:13 +0600

    creted a file called lecture7.js for return a function from another function

---
+++ Lecture/lecture7.js
@@ -0,0 +1,57 @@
+// Return a function from another function
+
+
+// In javascript "function" is just a value.
+
+function host(){
+    console.log('I am host function');
+    // return function(){
+    //     console.log('I am child function');
+    // }
+
+    // we can do that without anonymous function
+
+    function child(){
+        console.log('I am child function');
+    }
+
+    return child;
+}
+
+// we call called parent function and child function this way
+//host()();
+
+
+// this another way to call parent & child function at a time.
+
+var a= host();
+
+a();
+
+
+
+// New example
+
+function gretings(msg){
+    return function(name){
+        console.log(msg+' ' +name);
+    }
+}
+
+//method of calling return fuction 1.
+
+// var good = gretings('Good mornig');
+// var hello = gretings('Hello');
+
+// good('Redwan');
+// hello('World!');
+
+
+// method of calling return function 2.
+
+gretings('Hello')('World!');
+
+
+
+
+
